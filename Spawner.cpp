a1c66a1fe2d4964cab9506055bf01fce6567954c
lbaldev <166432608+lbaldev@users.noreply.github.com>
2025-06-23 23:46:12 -0300

    damageName

--- Spawner.cpp
+++ Spawner.cpp
@@ -22,31 +22,31 @@ void Spawner::spawnEnemies(std::vector<Enemy>& enemigos, sf::Vector2f playerPosi
 
         int tipo = std::rand() % 3;  // 0, 1 o 2
 
-        float vida, velocidad, daño;
+        float vida, velocidad, damage;
         std::string sprite;
 
         switch (tipo) {
         case 0: // Fantasma
             vida = 50.0f * (1 + timer / 60.0f);
             velocidad = 40.0f * (1 + timer / 60.0f);
-            daño = 10.0f * (1 + timer / 60.0f);
+            damage = 10.0f * (1 + timer / 60.0f);
             sprite = "assets/fantasma.png";
             break;
         case 1: // Araña
             vida = 30.0f * (1 + timer / 90.0f);
             velocidad = 30.0f;
-            daño = 15.0f * (1 + timer / 45.0f);
+            damage = 15.0f * (1 + timer / 45.0f);
             sprite = "assets/araña.png";
             break;
         case 2: // Bruto
             vida = 100.0f * (1 + timer / 45.0f);
             velocidad = 20.0f;
-            daño = 25.0f * (1 + timer / 45.0f);
+            damage = 25.0f * (1 + timer / 45.0f);
             sprite = "assets/bruto.png";
             break;
         }
 
-        enemigos.emplace_back(vida, velocidad, daño, sprite, sf::Vector2f(x, y));
+        enemigos.emplace_back(vida, velocidad, damage, sprite, sf::Vector2f(x, y));
 
         relojGeneracionEnemigos.restart();
     }
